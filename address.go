19337f5305e9c118d5f11f38aa84fbe6d3c73220
0xNyxaris⛓ <olaoyesalemgreat@gmail.com>
2025-02-20 00:51:25 +0100

    modified address.go

--- address.go
+++ address.go
@@ -7,11 +7,14 @@ import (
 )
 
 func main(){
- address := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+ address := common.HexToAddress("71c7656ec7ab88b098defb751b7401b5f6d8976f")
+
 
  fmt.Println(address.Hex())
+ fmt.Println(_address)
 
 //  fmt.Println(address.Hash().Hex())
+
 
  fmt.Println(address.Bytes())
 }
