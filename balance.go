191a5e72b4064a8d13f50e7baaa74d5652c149fb
0xNyxaris⛓ <olaoyesalemgreat@gmail.com>
2025-02-20 17:43:16 +0100

    Updated Comment

--- balance.go
+++ balance.go
@@ -18,14 +18,14 @@ func main() {
 	}
 
 	account  := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
-	balance , err :=  client.BalanceAt(context.Background(), account, nil)
+	balance,err :=  client.BalanceAt(context.Background(), account, nil)
 
 	if(err != nil){
 		log.Fatal(err)
 	}
 	fmt.Println(balance)
 
-	blocknumber:= big.NewInt(5532993)
+	 blocknumber:= big.NewInt(5532993)
 	balanceAt, err := client.BalanceAt(context.Background(), account, blocknumber)
 	
 	if(err != nil){
